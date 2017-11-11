2a7c45f9e3bbfb464c69b17b973efa8901a629e4
Pascal Wichmann <git@pa-w.de>
2017-11-11 22:35:11 +0100

    Make User.email unique

--- base/models.py
+++ base/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
-
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
 
 class User(AbstractUser):
+    email = models.EmailField(_('email address'), blank=True, unique=True)
+
     workhours_weekday = models.DecimalField(
         max_digits=4, decimal_places=2, default=8)
     workhours_weekend = models.DecimalField(
