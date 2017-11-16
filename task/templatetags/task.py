ca52d1518848b3c37f1dd2217d8086a2a92cf4a4
Pascal Wichmann <git@pa-w.de>
2017-11-16 09:52:51 +0100

    Implement more natural day template filter displaying weekdays for next week

--- task/templatetags/task.py
+++ task/templatetags/task.py
@@ -1,5 +1,9 @@
-from django import template
+from datetime import date, timedelta
+from typing import Union
 
+from django import template
+from django.contrib.humanize.templatetags.humanize import naturalday
+from django.utils.formats import date_format
 
 register = template.Library()
 
@@ -12,3 +16,12 @@ def multiply(value, arg):
 @register.filter
 def to_str(value) -> str:
     return str(value)
+
+
+@register.filter
+def more_natural_day(value: date, base_date: Union[None, date] = None) -> str:
+    if base_date is None:
+        base_date = date.today()
+    if timedelta(days=1) < value - base_date <= timedelta(days=6):
+        return date_format(value, 'l')
+    return naturalday(value)
