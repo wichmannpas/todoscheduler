ee72f38fe718f802d29572f32949ea4dc28ba327
Pascal Wichmann <git@pa-w.de>
2017-11-18 13:29:06 +0100

    Add ui tests for add and schedule of tasks

--- base/selenium_test.py
+++ base/selenium_test.py
@@ -1,5 +1,6 @@
 from subprocess import call, DEVNULL
 
+from django.contrib.auth import get_user_model
 from django.test.testcases import LiveServerTestCase
 from selenium import webdriver
 
@@ -23,3 +24,14 @@ class SeleniumTest(LiveServerTestCase):
     def tearDownClass(cls):
         cls.selenium.quit()
         super().tearDownClass()
+
+
+class AuthenticatedSeleniumTest(SeleniumTest):
+    def setUp(self):
+        self.selenium.get(self.live_server_url)
+        username_input = self.selenium.find_element_by_id("id_username")
+        username_input.send_keys('admin')
+        password_input = self.selenium.find_element_by_id("id_password")
+        password_input.send_keys('foobar123')
+        self.selenium.find_element_by_xpath('//input[@value="Login"]').click()
+        self.user = get_user_model().objects.get(username='admin')
