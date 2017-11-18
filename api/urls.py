2852f196621fbbf25eb17f2deed2a74167a63d5b
Pascal Wichmann <git@pa-w.de>
2017-11-18 15:18:44 +0100

    Add api view for scheduled tasks

--- api/urls.py
+++ api/urls.py
@@ -7,4 +7,5 @@ app_name = 'api'
 urlpatterns = [
     url(r'^token-auth/', authtoken_views.obtain_auth_token),
     url(r'^tasks/unscheduled/', views.unscheduled_tasks, name='unscheduled_tasks'),
+    url(r'^tasks/scheduled/', views.scheduled_tasks, name='scheduled_tasks'),
 ]
--- task/serializers.py
+++ task/serializers.py
@@ -1,12 +1,30 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
-from .models import Task
+from .models import Task, TaskExecution
 
 
-class TaskSerializer(ModelSerializer):
+class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = (
             'name',
             'duration',
         )
+
+
+class TaskExecutionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TaskExecution
+        fields = (
+            'task',
+            'day',
+            'day_order',
+            'duration',
+            'finished',
+        )
+    task = TaskSerializer()
+
+
+class DaySerializer(serializers.Serializer):
+    day = serializers.DateField()
+    executions = TaskExecutionSerializer(many=True)
