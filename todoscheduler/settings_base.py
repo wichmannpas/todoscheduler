7a45448828dbed84355da4e0df16c910e580c35f
Pascal Wichmann <git@pa-w.de>
2018-02-03 10:47:39 +0100

    Add api to INSTALLED_APPS (fixes #38)

--- todoscheduler/settings_base.py
+++ todoscheduler/settings_base.py
@@ -19,6 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Application definition
 
 INSTALLED_APPS = [
+    'api',
     'base',
     'task',
     'django.contrib.admin',
