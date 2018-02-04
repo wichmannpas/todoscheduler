c5786989a96b632266f74dcb9037a649569d56a5
Pascal Wichmann <git@pa-w.de>
2018-02-04 17:54:07 +0100

    Add basic vuejs client app with authentication functionality

---
+++ client/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app">
+    <div class="container">
+      <router-view>
+      </router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import LoginForm from './components/LoginForm'
+
+export default {
+  name: 'App',
+  components: {
+    LoginForm
+  }
+}
+</script>
---
+++ client/src/pages/LoginPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="login">
+    <h1>
+      Login
+    </h1>
+
+    <LoginForm />
+  </div>
+</template>
+
+<script>
+import LoginForm from '@/components/LoginForm'
+
+export default {
+  name: 'Login',
+  components: {
+    LoginForm
+  }
+}
+</script>
