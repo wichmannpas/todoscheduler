17799dd0e048dc0560f605999395f453b33a7b09
Pascal Wichmann <git@pa-w.de>
2018-02-10 18:53:40 +0100

    Add user api view

--- base/views.py
+++ base/views.py
@@ -1,6 +1,19 @@
 from django.conf import settings
 from django.http import HttpRequest, HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import UserSerializer
+
+
+class UserView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        return Response(
+            UserSerializer(request.user).data)
 
 
 def imprint(request: HttpRequest) -> HttpResponse:
