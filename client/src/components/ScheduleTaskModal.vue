ff238e4e745e7e254af0b361984df7ae63dc63af
Pascal Wichmann <git@pa-w.de>
2018-02-23 18:42:27 +0100

    Move schedule form to a separate component

--- client/src/components/ScheduleTaskModal.vue
+++ client/src/components/ScheduleTaskModal.vue
@@ -38,118 +38,43 @@
           </div>
         </div>
 
-        <form
-            @submit="scheduleTask">
-          <div class="content">
-            <div class="form-group">
-              <label class="form-label">
-                Schedule for
-              </label>
-              <select
-                  v-model="scheduleFor"
-                  v-bind:class="[
-                    { 'is-error': errors.indexOf('day') >= 0 }
-                  ]"
-                  class="form-select">
-                <option value="today">Today</option>
-                <option value="tomorrow">Tomorrow</option>
-                <option value="next_free_capacity">Next Free Capacity</option>
-                <option value="another_time">Another Time</option>
-              </select>
-              <input
-                  v-model="scheduleForDate"
-                  v-bind:class="[
-                    { 'is-error': errors.indexOf('day') >= 0 }
-                  ]"
-                  v-if="scheduleFor === 'another_time'"
-                  type="text"
-                  class="form-input"
-                  placeholder="Schedule for date"
-                  required />
-            </div>
-            <div
-                class="input-group">
-              <input
-                  ref="duration"
-                  v-model="duration"
-                  v-bind:class="[
-                    { 'is-error': errors.indexOf('duration') >= 0 }
-                  ]"
-                  type="number"
-                  class="
-                    align-right
-                    form-input"
-                    placeholder="Duration"
-                    step="0.01"
-                    required />
-              <span class="input-group-addon">h</span>
-
-            </div>
-            <div
-                v-if="loading"
-                class="loading loading-lg">
-            </div>
-          </div>
-          <div class="modal-footer">
-            <input
-                type="submit"
-                class="btn btn-primary"
-                value="Schedule" />
-            <button
-                @click="closeModal"
-                type="reset"
-                class="btn btn-link">
-              Cancel
-            </button>
-          </div>
-        </form>
+        <div class="content">
+          <ScheduleTaskForm
+            @complete="closeModal"
+            v-bind:task="task" />
+        </div>
+        <div class="modal-footer">
+          <input
+              @click="scheduleTask()"
+              type="submit"
+              class="btn btn-primary"
+              value="Schedule" />
+          <button
+              @click="closeModal"
+              type="reset"
+              class="btn btn-link">
+            Cancel
+          </button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Vue from 'vue'
-
-import Api from '@/api/Api'
-import { formatDayString } from '@/utils'
+import ScheduleTaskForm from '@/components/ScheduleTaskForm'
 
 export default {
   name: 'ScheduleTaskModal',
   props: [
     'task'
   ],
-  data: function () {
-    return {
-      loading: false,
-      scheduleFor: 'today',
-      scheduleForDate: formatDayString(new Date()),
-      duration: this.task.defaultScheduleDuration.toNumber(),
-      errors: []
-    }
-  },
-  mounted: function () {
-    this.$refs.duration.focus()
+  components: {
+    ScheduleTaskForm
   },
   methods: {
-    scheduleTask (event) {
-      event.preventDefault()
-      this.loading = true
-
-      let day = this.scheduleForDate
-      if (this.scheduleFor !== 'another_time') {
-        day = this.scheduleFor
-      }
-      Api.scheduleTask(this.$store, this.task, day, this.duration).then((response) => {
-        this.loading = false
-        Vue.set(this, 'errors', [])
-
-        this.closeModal()
-      }).catch((response) => {
-        this.loading = false
-
-        Vue.set(this, 'errors', Object.keys(response))
-      })
+    scheduleTask () {
+      this.$emit('schedule')
     },
     closeModal () {
       this.$emit('close')
