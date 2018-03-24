0fe8b78ae329d0e9d56f37accda38a11c096420f
Pascal Wichmann <git@pa-w.de>
2018-03-24 09:44:54 +0100

    Client: Add Task start attribute to model

--- client/src/models/Task.js
+++ client/src/models/Task.js
@@ -1,12 +1,17 @@
 import { Decimal } from 'decimal.js'
+import { parseDayString } from '@/utils'
 
-function Task (id, name, duration, scheduledDuration, finishedDuration, defaultScheduleDuration) {
+function Task (id, name, duration, scheduledDuration, finishedDuration, defaultScheduleDuration, start) {
   this.id = id
   this.name = name
   this.duration = Decimal(duration)
   this.scheduledDuration = Decimal(scheduledDuration)
   this.finishedDuration = Decimal(finishedDuration)
   this.defaultScheduleDuration = Decimal(defaultScheduleDuration)
+  this.start = start
+  if (start !== null) {
+    this.start = parseDayString(start)
+  }
 }
 Task.prototype.incompleteDuration = function () {
   return this.duration.sub(this.scheduledDuration)
@@ -25,7 +30,8 @@ function objectToTask (task) {
     task.duration,
     task.scheduled_duration,
     task.finished_duration,
-    task.default_schedule_duration)
+    task.default_schedule_duration,
+    task.start)
 }
 
 export {
