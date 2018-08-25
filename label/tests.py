e6011ad25171f9926b46e318eb656abdf803e2cb
Pascal Wichmann <git@pa-w.de>
2018-08-25 12:37:54 +0200

    Label: accept only valid color values (references #35)

--- label/tests.py
+++ label/tests.py
@@ -51,6 +51,68 @@ class LabelViewTest(AuthenticatedApiTest):
             label.color,
             '000000')
 
+    def test_create_label_invalid_color(self):
+        resp = self.client.post('/label/label/', {
+            'title': 'Test Label',
+            'description': 'A label description.',
+            'color': 'XZY123',
+        })
+        self.assertEqual(
+            resp.status_code,
+            status.HTTP_400_BAD_REQUEST)
+        self.assertSetEqual(
+            set(resp.data),
+            {
+                'color',
+            })
+        self.assertEqual(
+            Label.objects.count(),
+            0)
+
+        resp = self.client.post('/label/label/', {
+            'title': 'Test Label',
+            'description': 'A label description.',
+            'color': '',
+        })
+        self.assertEqual(
+            resp.status_code,
+            status.HTTP_400_BAD_REQUEST)
+        self.assertSetEqual(
+            set(resp.data),
+            {
+                'color',
+            })
+        self.assertEqual(
+            Label.objects.count(),
+            0)
+
+    def test_create_label_color_normalization(self):
+        resp = self.client.post('/label/label/', {
+            'title': 'Test Label',
+            'description': 'A label description.',
+            'color': 'F1F1FC',
+        })
+        self.assertEqual(
+            resp.status_code,
+            status.HTTP_201_CREATED)
+        label = Label.objects.first()
+        self.assertEqual(
+            label.color,
+            'f1f1fc')
+
+        resp = self.client.post('/label/label/', {
+            'title': 'Test Label',
+            'description': 'A label description.',
+            'color': 'f1f1fc',
+        })
+        self.assertEqual(
+            resp.status_code,
+            status.HTTP_201_CREATED)
+        label = Label.objects.first()
+        self.assertEqual(
+            label.color,
+            'f1f1fc')
+
     def test_update_label(self):
         """
         Test the update of an existing label using both
@@ -89,3 +151,44 @@ class LabelViewTest(AuthenticatedApiTest):
         self.assertEqual(
             label.description,
             'a long description of this label.')
+
+    def test_update_label_invalid_color(self):
+        label = Label.objects.create(
+            user=self.user,
+            title='Test Label',
+            color='111111')
+        resp = self.client.patch('/label/label/{}/'.format(label.id), {
+            'color': 'no valid color',
+        })
+        self.assertEqual(
+            resp.status_code,
+            status.HTTP_400_BAD_REQUEST)
+        self.assertSetEqual(
+            set(resp.data),
+            {
+                'color',
+            })
+        label.refresh_from_db()
+        self.assertEqual(
+            label.color,
+            '111111')
+
+        label = Label.objects.create(
+            user=self.user,
+            title='Test Label',
+            color='111111')
+        resp = self.client.patch('/label/label/{}/'.format(label.id), {
+            'color': 'ZZZZ11',
+        })
+        self.assertEqual(
+            resp.status_code,
+            status.HTTP_400_BAD_REQUEST)
+        self.assertSetEqual(
+            set(resp.data),
+            {
+                'color',
+            })
+        label.refresh_from_db()
+        self.assertEqual(
+            label.color,
+            '111111')
