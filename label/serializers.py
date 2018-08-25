44bd07810d1ae746d8b99c6c968c40fbcf1e9da0
Pascal Wichmann <git@pa-w.de>
2018-08-25 12:37:54 +0200

    Label: accept only valid color values (references #35)

--- label/serializers.py
+++ label/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from .models import Label
 
@@ -11,6 +12,16 @@ class LabelSerializer(serializers.ModelSerializer):
             'description',
             'color',
         )
+
+    def validate_color(self, value: str) -> str:
+        value = value.lower()
+
+        if len(value) != 6 or not all(
+                char in '0123456789abcdef'
+                for char in value):
+            raise ValidationError('not a valid color value')
+
+        return value
 
     def create(self, validated_data):
         validated_data['user'] = self.context['request'].user
